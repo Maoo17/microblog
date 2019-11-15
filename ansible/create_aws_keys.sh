aca3abf66f4be6a6cfa06ff6720ecf8bbc469bf0
Matilda <matildaytc@gmail.com>
2019-11-15 14:15:25 +0000

    Changed name of environment variable

--- ansible/create_aws_keys.sh
+++ ansible/create_aws_keys.sh
@@ -1,4 +1,4 @@
 sudo touch ansible/aws_keys.yml
 sudo echo “aws_access_key: $AWS_ACCESS_KEY” > ansible/aws_keys.yml
 sudo echo “aws_secret_key: $AWS_SECRET_KEY” >> ansible/aws_keys.yml
-sudo echo “aws_session_token: $AWS_SESSION_TOKEN” >> ansible/aws_keys.yml
+sudo echo “aws_security_token: $AWS_SECURITY_TOKEN” >> ansible/aws_keys.yml
