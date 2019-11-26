32a35211cdd10342c6dc6a8c1b4431de47c1a6a7
Matilda <matildaytc@gmail.com>
2019-11-26 15:10:13 +0000

    Added logfileaccess again

--- boot.sh
+++ boot.sh
@@ -8,8 +8,7 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn --statsd-host=localhost:9125 --statsd-prefix=gunicorn --bind :5000 microblog:app
+exec gunicorn --statsd-host=localhost:9125 --statsd-prefix=gunicorn --bind :5000 --access-logfile - --error-logfile - microblog:app
 
-# --access-logfile - --error-logfile -
 # exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
 # gunicorn --statsd-host=localhost:9125 --statsd-prefix=helloworld --bind 127.0.0.1:8080 hello:app
